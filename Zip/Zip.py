80bd25538ed972a7f9dd7d6f8ae4801968577f84
Sherman-Firefly <efracw2007@gmail.com>
2024-11-08 17:27:47 +0800

    Zip 1.1

--- Zip/Zip.py
+++ Zip/Zip.py
@@ -8,3 +8,8 @@ a2=[10,40,30,20,50,80,60,70,90]
 for x,y in zip(a1[::-1],a2[::-1]):
     print(x,y)
 
+d1=['reliance', 'infosys', 'tata']
+d2=[100,200,300]
+
+newd={d1:d2 for d1,d2 in zip(d1,d2)}
+print("\n",newd)
